04744d5eb5aa85888d6565842bc499d75c74d3ce
Mr. Habib <sirmrhabib@gmail.com>
2021-05-20 11:36:40 -0400

    Fixed unit tests

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-# Firstname, L
+# Lastname, Firstname
 # mm/dd/yyyy
 # Descriptive Title
 # Note: I will be marking your comments out of 2. Part of that is the inclusion of docstrings
 
-def selection_sort(arr):
+def selection_sort(array):
   pass
-  
-# Call your functions here to test them
+
+def main():
+    # Call your functions here to test them
+    test_list = [4, 3, 1, 2]
+    selection_sort(test_list)
+
+if __name__ == "__main__":
+    main()
+
--- main_test.py
+++ main_test.py
@@ -2,8 +2,15 @@
 from main import selection_sort
 from random import randint
 
-def test_selection_sort():
+def test_selection_sort_normal_1():
     assert selection_sort([3, 2, 1]) == [1, 2, 3]
-    assert selection_sort([2, 4, 6]) == [2, 4, 6]
+
+def test_selection_sort_normal_2():
+    assert selection_sort([1, 7, 5, 3]) == [1, 3, 5, 7]
+
+def test_selection_sort_one():
+    assert selection_sort([1]) == [1]
+
+def test_selection_sort_big():
     random_array = [randint(1, 100) for x in range(50)]
     assert selection_sort(random_array) == sorted(random_array)
